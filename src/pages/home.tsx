661e6015de50015742e6b4557dfda13fff3c4efe
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-12-02 01:11:51 +0100

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,4 @@
-import React from "react";
-
-const Home = () => {
+export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground">
       {/* Hero Section */}
@@ -68,6 +66,4 @@ const Home = () => {
       </footer>
     </div>
   );
-};
-
-export default Home;
+}
